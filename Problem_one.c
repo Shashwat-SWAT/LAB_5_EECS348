52916208e02f323de73d90a01c2e30453586a598
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-03-05 12:34:11 -0600

    sales summary function
    The void function finds the min, max, and the avg sales and prints them.

--- Problem_one.c
+++ Problem_one.c
@@ -52,28 +52,56 @@ char *which_month(int num_month) {
     }
 
     else {
-        return "Don't exist";
+        return "Error: Value is not right, month doesn't exist";
     }
 
 
 }
 
+void sales_summary (float mon_sales_ar[]) {
+    float max_month, max, min_month, min, avg = 0.0;
+    max = mon_sales_ar[0];
+    max_month = 0;
+    min = mon_sales_ar[0];
+    min_month = 0;
+
+    for (int i = 0; i<12; i++) {
+        if (max > mon_sales_ar[0]) {
+            max = mon_sales_ar[0];
+            max_month = i+1;
+        }
+        if (min < mon_sales_ar[0]) {
+            min = mon_sales_ar[0];
+            min_month = i+1;
+        }
+
+        avg += mon_sales_ar[0];
+    }
+
+    avg /= 12;
+
+    printf("Sales summary report:\n");
+    prinrf("Minimum sales: %.2f (%s)\n", min, which_month(min_month));
+    printf("Maximum sales: %.2f (%s)\n", max, which_month(max_month));
+    printf("Average sales: %.2f\n", avg);
+}
+
 int main() {
     float monthly_sales[12];
 
-    for (int i = 1; i <= 12; i++) {
+    for (int i = 0; i < 12; i++) {
         
-        printf("Enter your monthly sales for %s: ", which_month(i));
+        printf("Enter your monthly sales for %s: ", which_month(i+1));
         float temp;
         scanf("%f", &temp);
         monthly_sales[i] = temp;
     }
 
     printf("\n\n");
+    printf("Your monthly sales for 2026:\nMonth %10s", "Sales\n\n");
+    for (int i = 0; i < 12; i++) {
 
-    for (int i = 1; i <= 12; i++) {
-        printf("Your monthly sales for 2026:\nMonth %10s", "Sales");
-        printf("%s: %10.2f\n", which_month(i), monthly_sales[i]);
+        printf("%s: %10.2f\n", which_month(i+1), monthly_sales[i]);
     }
 
 }
