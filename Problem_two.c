4f3490f51f31615e4470d5ebcb80d0a47e91b0a8
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-03-08 16:01:32 -0500

    Adding all the the feature

--- Problem_two.c
+++ Problem_two.c
@@ -2,20 +2,63 @@
 
 #include <stdio.h>
 
-int main() {
-
-    int arr[SIZE][SIZE];
-    int sum = 0;
-
-    for (int i=0; i<SIZE; i++) {
-        for (int j=0; j<SIZE; j++) {
-            printf("Enter a number for position [%d][%d]: ", i, j);
-            scanf("%d", &arr[i][j]);
-            sum += arr[i][j];
+int addMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int result[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = m1[i][j] + m2[i][j];
         }
     }
+    return 0;
+}
 
-    printf("The sum of the numbers is: %d\n", sum);
+int multiplyMatrices(int m1[SIZE][SIZE], int m2[SIZE][SIZE], int result[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < SIZE; k++) {
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return 0;
+}
+
+int transposeMatrix(int matrix[SIZE][SIZE], int transposed[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    return 0;
+}
+
+void printMatrix(int matrix[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+
+    int m1[SIZE][SIZE] = {{1, 2, 3, 4, 5},{6, 7, 8, 9, 10},{11, 12, 13, 14, 15},{16, 17, 18, 19, 20},{21, 22, 23, 24, 25}};
+    int m2[SIZE][SIZE] = {{25, 24, 23, 22, 21},{20, 19, 18, 17, 16},{15, 14, 13, 12, 11},{10, 9, 8, 7, 6},{5, 4, 3, 2, 1}};
+    
+    int result[SIZE][SIZE];
+
+    addMatrices(m1, m2, result);
+    printf("Sum of matrices:\n");
+    printMatrix(result);
+
+    multiplyMatrices(m1, m2, result);
+    printf("Product of matrices:\n");
+    printMatrix(result);
+
+    transposeMatrix(m1, result);
+    printf("Transpose of matrix m1:\n");
+    printMatrix(result);
 
     return 0;
 }
